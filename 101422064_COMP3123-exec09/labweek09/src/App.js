e90b20466e07191eb5d820548bf080f24ae69d32
Pilkuk Edwin Chung <101422064@georgebrown.ca>
2024-10-31 17:50:41 -0400

    Lab Week 09 finished

---
+++ 101422064_COMP3123-exec09/labweek09/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to Full Stack Development - 1
+        </h1>
+        <h2>
+          React JS Programming Week09 Lab Exercise
+        </h2>
+        <h3>
+          101422064
+        </h3>
+        <h4>
+          Edwin Chung
+        </h4>
+        <p>
+          George Brown College, Toronto
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
